a79bc6e509f142e52fb6e6c15d60fb73a7e9bc13
nopnop2002 <>
2026-02-09 09:14:05 +0900

    Changed i2c initialization

--- calibrate/main/main.cpp
+++ calibrate/main/main.cpp
@@ -14,20 +14,13 @@ static const char *TAG = "MAIN";
 static const char *MDNS_HOSTNAME = "esp32";
 
 extern "C" {
-	void app_main(void);
-}
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 	void start_wifi(void);
 	void start_mdns(void);
 	int ws_server_start(void);
 	void server_task(void *pvParameters);
 	void client_task(void *pvParameters);
-#ifdef __cplusplus
+	void app_main(void);
 }
-#endif
 
 void hmc5883l(void *pvParameters);
 
@@ -57,7 +50,7 @@ void app_main(void)
 	start_mdns();
 
 	// Initialize i2c
-	I2Cdev::initialize(400000);
+	I2Cdev::initialize();
 
 	// Create Message Buffer
 	xMessageBufferToClient = xMessageBufferCreate(1024);
